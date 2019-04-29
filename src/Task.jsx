e74b1ba3319f66a6eb8e61c139897627caedbd0c
Tony Russo <tony@tortugacoders.com>
2019-04-29 11:16:42 +0700

    call function from button

--- src/Task.jsx
+++ src/Task.jsx
@@ -8,10 +8,15 @@ class Task extends Component {
 		task: this.props.task
 	}
 
+	// functions
+	test = (_id) => alert(_id)
+
+
 	// renders html
 	render() {
 		return (
 			<li className="list-group-item">
+				<button className="btn btn-primary btn-sm mr-3" onClick={() => this.test(this.state.task._id)}>-</button>
 				<span className={this.state.task.done ? 'done' : ''} style={styles.span}>{this.state.task.name}</span>
 			</li>
 		)
